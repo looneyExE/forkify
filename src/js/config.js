f4b8231567c16fb656b58ad511cc49670370df85
looooney <lvpal03@mail.ru>
2021-12-17 16:54:13 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "19ced378-32b6-4854-b2a2-ebc6b8d33abe";
+export const MODAL_CLOSE_SEC = 2.5;
